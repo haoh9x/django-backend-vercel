f648c4d111325f1e6860ef0b804a3ec85d09926e
haoh9x <161196792+haoh9x@users.noreply.github.com>
2024-10-14 15:37:23 +0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('auth/', include("djoser.urls")),
     path('auth/', include("djoser.urls.jwt")),
     re_path(r'^auth/', include('djoser.social.urls')),
-
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
